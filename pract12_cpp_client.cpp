14a84ccc9ee0470a16cfcac2b13500ca60bade6d
BNKTOP IIIecTakoB <viktorsestakov431@gmail.com>
2026-02-19 14:03:03 +0500

    Add files via upload

---
+++ pract12_cpp_client.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Клиент запущен!" << endl << endl;
+
+	const char* mName = "Printer";
+	HANDLE hMut = OpenMutexA(MUTEX_ALL_ACCESS, FALSE, mName);
+	if (hMut == NULL) {
+		cout << "Ошибка октрытия мьютекса!" << endl;
+		return GetLastError();
+	}
+
+	WaitForSingleObject(hMut, INFINITE);
+	cout << "Отпралвено на печать!" << endl;
+	ReleaseMutex(hMut);
+	cout << "Ожидание завершения печати!" << endl;
+	WaitForSingleObject(hMut, INFINITE);
+	cout << "Печать завершена!" << endl;
+	ReleaseMutex(hMut);
+
+	CloseHandle(hMut);
+	return 0;
+}
