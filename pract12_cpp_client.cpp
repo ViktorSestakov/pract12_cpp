cc7aa483931be0ac085d8811d43bf263f72e1712
BNKTOP IIIecTakoB <viktorsestakov431@gmail.com>
2026-03-04 09:51:16 +0500

    Update pract12_cpp_client.cpp

--- pract12_cpp_client.cpp
+++ pract12_cpp_client.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <windows.h>
 
 using namespace std;
@@ -7,23 +7,61 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
+	int vibor = 0;
+	DWORD prior;
+	cout << "Выберите приоритет запускаемого клиента:" << endl;
+	cout << "1. Фоновый" << endl;
+	cout << "2. Нормальный" << endl;
+	cout << "3. Высокий" << endl;
+	cout << "Выбор: ";
+	cin >> vibor;
+
+	switch (vibor) {
+	case 1: prior = IDLE_PRIORITY_CLASS;
+		break;
+	case 2: prior = NORMAL_PRIORITY_CLASS; 
+		break;
+	case 3: prior = HIGH_PRIORITY_CLASS; 
+		break;
+	default: {
+		cout << "Ошибка выбора!" << endl;
+		cout << "окно закроется через 2 секунды" << endl;
+		Sleep(2000);
+		return 0;
+		break;
+		}
+	}
+
+	if (!SetPriorityClass(GetCurrentProcess(), prior)) {
+		cout << "Ошибка установки приоритета №" << vibor << endl;
+		return GetLastError();
+	}
+	else {
+		cout << "Приорите установлен!" << endl;
+		Sleep(100);
+	}
+
+	system("cls");
 	cout << "Клиент запущен!" << endl << endl;
 
-	const char* mName = "Printer";
+	const char* mName = "PrinterMutMut";
 	HANDLE hMut = OpenMutexA(MUTEX_ALL_ACCESS, FALSE, mName);
-	if (hMut == NULL) {
-		cout << "Ошибка октрытия мьютекса!" << endl;
+	const char* sName = "PrinterSemaphore";
+	HANDLE hSem = OpenSemaphoreA(SEMAPHORE_ALL_ACCESS, FALSE, sName);
+
+	if (hMut == NULL || hSem == NULL) {
+		cout << "Ошибка создания мьютекса" << endl;
 		return GetLastError();
 	}
 
 	WaitForSingleObject(hMut, INFINITE);
-	cout << "Отпралвено на печать!" << endl;
 	ReleaseMutex(hMut);
-	cout << "Ожидание завершения печати!" << endl;
-	WaitForSingleObject(hMut, INFINITE);
-	cout << "Печать завершена!" << endl;
-	ReleaseMutex(hMut);
+	ReleaseSemaphore(hSem, 1, NULL);
 
 	CloseHandle(hMut);
+	CloseHandle(hSem);
+	system("cls");
+	cout << "Печать отправлена успешно! (окно закроется через 2 секунды)" << endl;
+	Sleep(2000);
 	return 0;
 }
